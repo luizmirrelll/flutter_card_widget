a32855fd94574455f138344a9118fdecef85ca11
luizmirrelll <aloysius.mirel96@gmail.com>
2020-10-28 13:33:39 +0700

    card widget

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        body: Container(
+          margin: EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              buildCard(Icons.account_box, "Account Box"),
+              buildCard(Icons.adb, "serangga Android")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Card buildCard(IconData iconData, String text) {
+    return Card(
+      child: Row(
+        children: <Widget>[
+          Container(
+              margin: EdgeInsets.all(5),
+              child: Icon(
+                iconData,
+                color: Colors.green,
+              )),
+          Text("Account Box")
+        ],
+      ),
+    );
+  }
+}
